66daaa4dc51cf521535337044aef6208b898e7ae
tyautyau56 <i19yanagi@gmail.com>
2020-09-02 09:43:59 +0900

    up install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.zprofile $HOME/.zprofile
+ln -s $HOME/dotfiles/.zshenv $HOME/.zshenv
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/tmux/.tmux.conf $HOME/.tmux.conf
+
